10cf4040f92dd1d710dcd7a5f23ebe0b68ad8648
sspangsberg <soren@spangsberg.net>
2016-02-11 11:45:10 +0100

    Initial import

---
+++ Profiling/AlgoritmsFaceOff.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Profiling
+{
+    public class AlgoritmsFaceOff
+    {
+        const int numberOfIterations = 100000;
+
+        private static void Algorithm1()
+        {
+            string result = "";
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                result += 'a';
+
+            }
+        }
+        private static void Algorithm2()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                sb.Append('a');
+
+            }
+
+            string result = sb.ToString();
+        }
+
+        public static void StartShowOff()
+        {            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Algorithm1();
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+            Algorithm2();
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed);
+            Console.WriteLine("Ready...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Profiling/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Profiling
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void DisplaySimpleMemoryInfomation()
+        {
+            Console.WriteLine("Press escape key to stop");
+
+            using (PerformanceCounter pc = new PerformanceCounter("Processor", "% Processor Time"))
+            {
+                string text = "Available Memory:";
+
+                Console.Write(text);
+
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.Write(pc.RawValue);
+                        Console.SetCursorPosition(text.Length, Console.CursorTop);
+                    }
+                }
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void TestCustomPerformanceCounters()
+        {
+            if (CreatePerformanceCounters())
+            {
+                Console.WriteLine("Created performance counters");
+                Console.WriteLine("Please restart application");
+                Console.ReadKey();
+                return;
+            }
+
+            var totalOperationsCounter = new PerformanceCounter("MyCategory", "# operations executed", "", false);
+            var operationsPerSecondCounter = new PerformanceCounter("MyCategory", "# operations / sec", "", false);
+
+            totalOperationsCounter.Increment();
+            operationsPerSecondCounter.Increment();
+        }
+
+        static bool CreatePerformanceCounters()
+        {
+            if (!PerformanceCounterCategory.Exists("MyCategory"))
+            {
+                CounterCreationDataCollection counters = new CounterCreationDataCollection
+                {
+                    new CounterCreationData("# operations executed","Total number of operations executed",PerformanceCounterType.NumberOfItems32),
+                    new CounterCreationData("# operations / sec","Number of operations executed per second",PerformanceCounterType.RateOfCountsPerSecond32),
+                };
+
+                PerformanceCounterCategory.Create("MyCategory", "Sample category for Codeproject", counters);
+
+                return true;
+            }
+
+            return false;
+        }
+               
+
+        /// <summary>
+        /// Displays menu and handles user input
+        /// </summary>
+        public static void Start()
+        {
+            int choice = -1;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("1: Profiling ShowOff Demo");
+                Console.WriteLine("2: Simple Performance Counter (Memory) Demo");
+                Console.WriteLine("3: Custom Performance Counters Demo");
+                
+
+                Console.WriteLine("0: EXIT");
+
+                Console.Write("\nChose a number...");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        ResetConsole("Kører 'Profiling ShowOff Demo'...\n");
+                        AlgoritmsFaceOff.StartShowOff();
+                        break;
+                    case 2:
+                        ResetConsole("Kører 'Simple Performance Counter (Memory)'...\n");
+                        DisplaySimpleMemoryInfomation();
+                        break;
+                    case 3:
+                        ResetConsole("Kører 'Custom Performance Counters Demo'...\n");
+                        TestCustomPerformanceCounters();
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+            while (choice != 0);
+
+        }
+
+
+        public static void ResetConsole(string text)
+        {
+            Console.Clear();
+            Console.WriteLine(text);
+        }
+    }
+}
